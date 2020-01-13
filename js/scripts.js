d19eb432b6ffa68ec03993959056afa4f5b257b6
Jason Stocks and Todd Walraven <jason@>
2020-01-13 13:19:26 -0800

    add calculator

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,29 @@ var add = function(number1, number2) {
   var number2 = parseInt(prompt("enter second number:"));
   alert(add(number1, number2));
   
+var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  var number1 = parseInt(prompt("enter a number dude:"));
+  var number2 = parseInt(prompt("enter second number:"));
+  alert(subtract(number1, number2));
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  
+  var number1 = parseInt(prompt("enter a number dude:"));
+  var number2 = parseInt(prompt("enter second number:"));
+  alert(multiply(number1, number2));
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+  
+  var number1 = parseInt(prompt("enter a number dude:"));
+  var number2 = parseInt(prompt("enter second number:"));
+  alert(divide(number1, number2));
+
+
+  
