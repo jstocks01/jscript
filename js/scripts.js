7550f26f3fd0ef0c2ba99b81ed4df12cad4a2e6f
Jason Stocks and Todd Walraven <todd@>
2020-01-13 11:56:45 -0800

    commit before lunch

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  var number1 = parseInt(prompt("enter a number dude:"));
+  var number2 = parseInt(prompt("enter second number:"));
+  alert(add(number1, number2));
+  
